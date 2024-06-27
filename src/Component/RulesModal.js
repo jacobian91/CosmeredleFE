887ccfc71b85e29e47bdc0155f4b67d90da0ae8c
Kelvin <prussiakelvin@gmail.com>
2024-06-27 22:47:26 +0100

    Updated rules modal styling

--- src/Component/RulesModal.js
+++ src/Component/RulesModal.js
@@ -17,22 +17,25 @@ export default function RulesModal() {
             <div className="modal-overlay" onClick={toggleModal} />
             <div className="modal-content rules-modal">
               <h1 className="modal-title">How to play</h1>
-              <p className="modal-text rules-text">
-                <span className="incorrect-text">Red</span> = Guess is incorrect
+              <div className="modal-text rules-text">
+                <span><span className="incorrect-text">Red</span> = Guess is incorrect</span>
                 <br/>
-                <span className="partial-text">Orange</span> = Guess is partially correct (only applies to species and abilities/investiture) e.g.
+                  <span><span className="partial-text">Orange</span> = Guess is partially correct (only applies to species and abilities/investiture) e.g.</span>
                 <br/>
                 <div className="example-container">
-                    <img className="example-img" src="/images/species-partial.png" alt="Species example"/> Character is Human but not Skaa
+                  <img className="example-img" src="/images/species-partial.png" alt="Species example"/>
+                  <span>Character is Human but not Skaa</span>
                 </div>
                 <br/>
                 <div className="example-container">
-                    <img className="example-img" src="/images/abilities-partial.png" alt="Abilities example"/> At least one of the abilities/investitures is correct
+                  <img className="example-img" src="/images/abilities-partial.png" alt="Abilities example"/>
+                  <span>At least one of the abilities/investitures is correct</span>
                 </div>
-                <span className="correct-text">Green</span> = Guess is correct
                 <br/>
-                All information is taken from <a className="external-link" href="https://coppermind.net">Coppermind</a>
-              </p>
+                <span><span className="correct-text">Green</span> = Guess is correct</span>
+                <br/>
+                <span>All information is taken from <a className="external-link" href="https://coppermind.net">Coppermind</a></span>
+              </div>
             </div>
           </div>
         )}
